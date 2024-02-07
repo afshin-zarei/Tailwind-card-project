40f03f54cc6b03d41d627bef72beb2fe252e7f3a
Afshin Zarei <afshin.zarei2000@gmail.com>
2024-02-07 22:23:23 +0330

    add card project

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/index.html"],
+  theme: {
+    fontFamily: {
+      "inter": "Inter"
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
